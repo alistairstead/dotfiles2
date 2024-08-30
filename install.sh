c0b23e2e6f26124d4af4187bcefded7eb7435ef4
alistairstead <alistair_stead@me.com>
2024-08-30 21:54:26 +0100

    run in sudo

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
-echo "Installing personal dotfiles..."
-git clone git@github.com:alistairstead/dotfiles2.git ~/dotfiles
+#!/usr/bin/env sh
 
-cd ~/dotfiles || exit
+echo "Mac OS Install Setup Script"
+
+sudo -v
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
+
+echo "Installing personal dotfiles..."
+
+git clone https://github.com/alistairstead/dotfiles2.git ~/dotfiles
+
+cd ~/dotfiles || echo "Cloning dotfiles failed" exit
 
 echo "Installing nix..."
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
