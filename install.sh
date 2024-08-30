353781678346297eba2a75ec232e59462e09bb4e
alistairstead <alistair_stead@me.com>
2024-08-30 21:34:06 +0100

    mak nix available after install

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
 
+source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+which nix
+
+nix --version
+
 nix run nix-darwin --experimental-feature nix-command --experimental-feature flakes -- switch --flake ./nix-darwin
