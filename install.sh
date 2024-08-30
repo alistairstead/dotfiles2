ffc6d5191eacf9ebc7e2bac17d36f6d9fa467160
alistairstead <alistair_stead@me.com>
2024-08-30 20:20:30 +0100

    add --no-confirm flag

--- install.sh
+++ install.sh
@@ -1,3 +1,1 @@
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix >nix-installer.sh
-chmod +x nix-installer.sh
-./nix-installer.sh install
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
