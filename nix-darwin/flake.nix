6467b243d92969301af73bd5537006833bca71ff
alistairstead <alistair_stead@me.com>
2024-09-06 19:03:31 +0100

    sync

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -79,7 +79,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.${username} = import ./home.nix { inherit username; inherit pkgs; };
+            home-manager.users.${username} = import ./home.nix { inherit username; };
           }
         ];
       };
