272bfe9847e3370e7cf65bcaec0970a51f584448
alistairstead <alistair_stead@me.com>
2024-09-06 12:23:11 +0100

    update homebrew install

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ end
 # Check for Homebrew
 if test ! "$(which brew)"; then
   echo "INFO: Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew upgrade
