92ca04cbbf282da201f62d3be63fe9c8be038c24
alistairstead <alistair_stead@me.com>
2024-09-11 18:05:33 +0100

    sync versions

--- hosts/wombat/default.nix
+++ hosts/wombat/default.nix
@@ -21,19 +21,16 @@ inputs.darwin.lib.darwinSystem {
       # atuin.enable = true;
       # charm.enable = true;
       # neovim.enable = true;
-      # discord.enable = true;
-      # firefox.enable = true;
+      discord.enable = true;
       dotfiles.enable = true;
       terraform.enable = true;
       # python.enable = true;
       # rust.enable = true;
       # lua.enable = true;
       obsidian.enable = true;
-      # kubernetes.enable = true;
       _1password.enable = true;
       slack.enable = true;
       wezterm.enable = true;
-      # yt-dlp.enable = true;
     }
   ];
 }
